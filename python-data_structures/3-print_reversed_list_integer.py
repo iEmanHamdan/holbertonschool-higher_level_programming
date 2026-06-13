7925d41a4494bf00252bfcc7883ebc0e3c42d5fa
noname? <eman.hamdan@outlook.sa>
2026-06-13 19:49:28 +0000

    Task 3: Add function to print list integers in reverse order

---
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def print_reversed_list_integer(my_list=[]):
+    """Prints all integers of a list in reverse order, one per line."""
+    if my_list:
+        for number in my_list[::-1]:
+            print("{:d}".format(number))
