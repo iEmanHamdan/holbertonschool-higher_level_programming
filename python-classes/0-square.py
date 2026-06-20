680dec494a0a19530e953ec2b53f898e1e725e2e
SillyMantu <eman.hamdan@outlook.sa>
2026-06-20 19:17:35 +0000

    Task 0: Create an empty Square class

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -8,7 +8,7 @@ The purpose is to introduce the basics of Object-Oriented Programming.
 class Square:
     """
     An empty class that defines a square.
-    
+
     This class currently has no attributes or methods, functioning
     as a base blueprint for future tasks.
     """
