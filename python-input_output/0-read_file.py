a7bb9e04d1099dffc430a12292f45febf7ccc348
iEmanHamdan <eman.hamdan@outlook.sa>
2026-07-04 23:49:26 +0300

    g

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,5 +5,4 @@
 def read_file(filename=""):
     """Reads a text file and prints it to stdout."""
     with open(filename, encoding="utf-8") as f:
-        print(f.read(), end="")
-        
+        print(f.read(), end="")     
